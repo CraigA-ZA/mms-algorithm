9cc92ef9693dba34534fdeb986527435fcd52e06
Craig Armstrong <craig.armstrong@entelect.co.za>
2024-02-06 12:55:19 +0200

    Added usage hint if missing argument

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,14 @@
 import sys
 
 #Take array from command line argument
-array = sys.argv[1].split(',')
+if len(sys.argv)  != 2:
+    sys.exit("Usage: python algorithm.py <votes_array>")
+
+votes = sys.argv[1].split(',')
 counter = 0
 candidate = None
 
-for number in array:
+for number in votes:
     if counter == 0:
         #Start counting a new candidate
         candidate = number
@@ -18,8 +21,8 @@ for number in array:
         counter -= 1
 
 #Confirm that the candidate is truly a majority
-count_candidate = array.count(candidate)
-if count_candidate > len(array) // 2:
+count_candidate = votes.count(candidate)
+if count_candidate > len(votes) // 2:
     print(candidate)
 else:
     print("No majority element")
